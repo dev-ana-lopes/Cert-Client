581314899f00484ac40deba933ab169c9973f2e7
dev-ana-lopes <dev.anajulia@outlook.com.br>
2024-04-08 21:37:10 -0300

    feature:estilização da página index com ajustes no botão e centralização da div.

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 560,
+    frame: false, 
+    
+    webPreferences: {
+      nodeIntegration: true 
+    }
+  });
+
+
+  mainWindow.loadFile('index.html');
+  
+  if (process.platform === 'win32') {
+    mainWindow.setMenu(null); 
+  } else if (process.platform === 'linux') {
+    console.log("Linux");
+  }
+}
+
+app.whenReady().then(createWindow);
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+   if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+

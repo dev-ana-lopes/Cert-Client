67007d3b94ebbd6ddc8607391f327f4f8e0f67e0
D13YSON <dieysonjason@gmail.com>
2024-05-26 16:51:41 -0300

    Feature 12: Ajuste no z-index e validações para proxima pagina

--- tipo-certificado/tipo-certificado.js
+++ tipo-certificado/tipo-certificado.js
@@ -7,4 +7,45 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-   
+document.getElementById('btn-proximo').addEventListener('click', function(event) {
+    const selectedValue = document.querySelector('input[name="opcao"]:checked').value;
+    localStorage.setItem('selectedOption', selectedValue);
+    const link = this.querySelector('a');
+    window.location.href = link.href;
+});
+
+function logCheckboxClick(checkbox) {
+    const checkboxValue = checkbox;
+    const check1 = document.getElementById('check');
+    const check2 = document.getElementById('check2');
+
+    if (checkboxValue === 'pfx-crtkey') {
+        if (!check2.disabled) {
+            check2.disabled = true; 
+        } else {
+            check2.disabled = false; 
+        }
+    } else if (checkboxValue === 'crtkey-pfx') {
+        if (!check1.disabled) {
+            check1.disabled = true; 
+        } else {
+            check1.disabled = false; 
+        }
+    }
+    validate();
+}
+
+function validate() {
+    const acceptedTerms = document.getElementById('aceito_termos');
+    const check1 = document.getElementById('check');
+    const check2 = document.getElementById('check2');
+    const btnNext = document.getElementById('btn-proximo');
+
+    if ((check1.checked || check2.checked) && acceptedTerms.checked) {
+        btnNext.classList.add('enabled');
+        btnNext.disabled = false; 
+    } else {
+        btnNext.classList.remove('enabled');
+        btnNext.disabled = true;
+    }
+}
--- upload/upload.js
+++ upload/upload.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const dropZone = document.getElementById('drop-zone');
     const fileInput = document.getElementById('file-input');
     const fileUploadText = document.getElementById('file-upload-text');
-    window.location;
 
     dropZone.addEventListener('click', () => {
         fileInput.click();
@@ -33,6 +32,18 @@ document.addEventListener('DOMContentLoaded', function() {
             const file = files[0];
             fileUploadText.innerHTML = `<h4>Arquivo "${file.name}" selecionado.</h4>`;
             console.log(file);
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const selectedOption = localStorage.getItem('selectedOption');
+    
+    if (selectedOption) {
+        if (selectedOption === 'pfx-crtkey') {
+            console.log('Opção selecionada: PFX para CRT + KEY');
+        } else if (selectedOption === 'crtkey-pfx') {
+            console.log('Opção selecionada: CRT + KEY para PFX');
         }
     }
 });
